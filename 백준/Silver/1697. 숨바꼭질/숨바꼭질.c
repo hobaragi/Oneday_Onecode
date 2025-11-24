a2da0a3261c709d94168681e9905faec2ef60a44
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-11-24 13:00:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2560 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define MAX 100001
+
+int N, K;
+int visited[MAX];
+
+typedef struct {
+	int x, time;
+}Node;
+
+Node queue[MAX];
+int front = 0, rear = 0;
+
+void push(int x, int time) {
+	queue[rear].x = x;
+	queue[rear].time = time;
+	rear++;
+}
+
+Node pop() {
+	return queue[front++];
+}
+
+int isEmpty() {
+	return front == rear;
+}
+
+int main()
+{
+	int i = 0, min = MAX;
+	int cnt[MAX];
+
+	scanf("%d %d", &N, &K);
+
+	if (N == K) {
+		printf("0");
+		return 0;
+	}
+	else if (N > K) {
+		printf("%d", N - K);
+		return 0;
+	}
+
+	visited[N] = 1;
+
+	push(N, 0);
+
+	while (!isEmpty()) {
+		Node cur = pop();
+		int x = cur.x;
+		int time = cur.time;
+		int nx, nextTime = time + 1;;
+
+		if (x == K) {
+			cnt[i++] = time;
+		}
+
+		nx = x - 1;
+		if (0 <= nx && !visited[nx]) {
+			push(nx, nextTime);
+			visited[nx] = 1;
+		}
+
+		nx = x + 1;
+		if (nx < MAX && !visited[nx]) {
+			push(nx, nextTime);
+			visited[nx] = 1;
+		}
+
+		nx = 2 * x;
+		if (nx < MAX && !visited[nx]) {
+			push(nx, nextTime);
+			visited[nx] = 1;
+		}
+	}
+
+	for (int j = 0; j < i; j++) {
+		if (min > cnt[j])
+			min = cnt[j];
+	}
+
+	printf("%d", min);
+
+	return 0;
+}
