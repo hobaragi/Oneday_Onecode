07cbfcf8c4a7555b2c0f8f98dd7e7586c693470d
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-11-25 01:45:19 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.c
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.c
@@ -1,174 +1,87 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 
 int gear[4][8];
 
-// n=방향, m=기어 번호
-void rotate(int n, int m) {
+void change(int num, int way) {
 	int temp;
-	if (n == 1) {
-		temp = gear[m][7];
-		for (int i = 7; i > 0; i--)
-			gear[m][i] = gear[m][i - 1];
-		gear[m][0] = temp;
+	if (way == 1) {
+		temp = gear[num][7];
+		for (int i = 7; i > 0; i--) {
+			gear[num][i] = gear[num][i - 1];
+		}
+		gear[num][0] = temp;
 	}
-	else if (n == -1) {
-		temp = gear[m][0];
-		for (int i = 0; i < 7; i++)
-			gear[m][i] = gear[m][i + 1];
-		gear[m][7] = temp;
+	else if (way == -1) {
+		temp = gear[num][0];
+		for (int i = 0; i < 7; i++) {
+			gear[num][i] = gear[num][i + 1];
+		}
+		gear[num][7] = temp;
 	}
 }
 
 int main()
 {
-	int K, num, way;
-	int temp, sum = 0;
-	int result[3];
+	int K, num, way, score = 0;
+	int check[3];
 
-	// 12시 방향부터 톱니바퀴 상태 입력
-	// N극 = 0, S극 = 1
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 8; j++) {
 			scanf("%1d", &gear[i][j]);
 		}
 	}
 
-	// 회전 횟수 입력
+	for (int i = 0; i < 3; i++) {
+		if (gear[i][2] == gear[i + 1][6])
+			check[i] = 0;
+		else
+			check[i] = 1;
+	}
+
 	scanf("%d", &K);
 
-	for (int i = 0; i < K; i++) {
+	int rotate[4];
+
+	for (int cmd = 0; cmd < K; cmd++) {
 		scanf("%d %d", &num, &way);
+		num--; // 0-based로 변환
 
-		// 다른 극인지 확인
-		for (int j = 0; j < 3; j++) {
-			if (gear[j][2] != gear[j+1][6])
-				result[j] = 1;
-			else
-				result[j] = 0;
+		// 1. 인접 극 확인
+		for (int i = 0; i < 3; i++) {
+			check[i] = (gear[i][2] != gear[i + 1][6]);
 		}
 
-		// 1번 기어
-		if (num == 1) {
-			if (way == 1) {
-				rotate(1, 0);
-				if (result[0] == 1) {
-					rotate(-1, 1);
-					if (result[1] == 1) {
-						rotate(1, 2);
-						if (result[2] == 1) {
-							rotate(-1, 3);
-						}
-					}
-				}
-			}
-			else {
-				rotate(-1, 0);
-				if (result[0] == 1) {
-					rotate(1, 1);
-					if (result[1] == 1) {
-						rotate(-1, 2);
-						if (result[2] == 1) {
-							rotate(1, 3);
-						}
-					}
-				}
-			}
+		// 2. 회전 배열 초기화
+		for (int i = 0; i < 4; i++) rotate[i] = 0;
+		rotate[num] = way;
+
+		// 3. 왼쪽으로 전달
+		for (int i = num - 1; i >= 0; i--) {
+			if (check[i] == 1) rotate[i] = -rotate[i + 1];
+			else break;
 		}
-		// 2번 기어
-		else if (num == 2) {
-			if (way == 1) {
-				rotate(1, 1);
-				if (result[0] == 1) {
-					rotate(-1, 0);
-				}
-				if (result[1] == 1) {
-					rotate(-1, 2);
-					if (result[2] == 1) {
-						rotate(1, 3);
-					}
-				}
-			}
-			else {
-				rotate(-1, 1);
-				if (result[0] == 1) {
-					rotate(1, 0);
-				}
-				if (result[1] == 1) {
-					rotate(1, 2);
-					if (result[2] == 1) {
-						rotate(-1, 3);
-					}
-				}
-			}
+
+		// 4. 오른쪽으로 전달
+		for (int i = num; i < 3; i++) {
+			if (check[i] == 1) rotate[i + 1] = -rotate[i];
+			else break;
 		}
-		// 3번 기어
-		else if (num == 3) {
-			if (way == 1) {
-				rotate(1, 2);
-				if (result[2] == 1) {
-					rotate(-1, 3);
-				}
-				if (result[1] == 1) {
-					rotate(-1, 1);
-					if (result[0] == 1) {
-						rotate(1, 0);
-					}
-				}
-			}
-			else {
-				rotate(-1, 2);
-				if (result[2] == 1) {
-					rotate(1, 3);
-				}
-				if (result[1] == 1) {
-					rotate(1, 1);
-					if (result[0] == 1) {
-						rotate(-1, 0);
-					}
-				}
-			}
-		}
-		// 4번 기어
-		else if (num == 4) {
-			if (way == 1) {
-				rotate(1, 3);
-				if (result[2] == 1) {
-					rotate(-1, 2);
-					if (result[1] == 1) {
-						rotate(1, 1);
-						if (result[0] == 1) {
-							rotate(-1, 0);
-						}
-					}
-				}
-			}
-			else {
-				rotate(-1, 3);
-				if (result[2] == 1) {
-					rotate(1, 2);
-					if (result[1] == 1) {
-						rotate(-1, 1);
-						if (result[0] == 1) {
-							rotate(1, 0);
-						}
-					}
-				}
-			}
+
+		// 5. 실제 회전
+		for (int i = 0; i < 4; i++) {
+			if (rotate[i] != 0) change(i, rotate[i]);
 		}
 	}
 
-	if (gear[0][0] == 1)
-		sum += 1;
-	if (gear[1][0] == 1)
-		sum += 2;
-	if (gear[2][0] == 1)
-		sum += 4;
-	if (gear[3][0] == 1)
-		sum += 8;
 
-	printf("%d", sum);
+	for (int i = 0; i < 4; i++) {
+		if (gear[i][0] == 1) {
+			score += (1 << i);   // 1, 2, 4, 8
+		}
+	}
+
+	printf("%d", score);
 
 	return 0;
 }
