b7ae29793ca6cb300120471a7da7c596ee35fc7a
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-11-25 00:54:24 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.c
+++ 백준/Bronze/1546. 평균/평균.c
@@ -1,26 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 
 int main()
 {
-	int N, M;
-	float score[1000];
-	float avg = 0, max = 0;
+	int N, M[1000];
+	float max = 0, sum = 0;
 
 	scanf("%d", &N);
 
 	for (int i = 0; i < N; i++) {
-		scanf("%d", &score[i]);
-		if (max < score[i])
-			max = score[i];
+		scanf("%d", &M[i]);
+		if (max < M[i])
+			max = M[i];
 	}
 
 	for (int i = 0; i < N; i++) {
-		avg += score[i] / max * 100;
+		sum += M[i] / max * 100;
 	}
 
-	printf("%f", avg/N);
+	printf("%f", sum / N);
 
 	return 0;
 }
