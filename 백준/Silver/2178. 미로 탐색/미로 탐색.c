9f41c1ec833ad9a4bb4c87896dd60a0eec57562d
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-11-26 16:21:59 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 1272 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.c
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.c
@@ -4,11 +4,10 @@
 
 int N, M;
 int maze[MAX][MAX];
-int visited[MAX][MAX];
 int dist[MAX][MAX];
 
-int dx[4] = { 1,-1,0,0 };
-int dy[4] = { 0,0,1,-1 };
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
 
 typedef struct {
 	int x, y;
@@ -17,9 +16,9 @@ typedef struct {
 Node queue[MAX * MAX];
 int front = 0, rear = 0;
 
-void push(int x, int y) {
-	queue[rear].x = x;
+void push(int y, int  x) {
 	queue[rear].y = y;
+	queue[rear].x = x;
 	rear++;
 }
 
@@ -33,15 +32,17 @@ int isEmpty() {
 
 int main()
 {
+	// 크기 입력
 	scanf("%d %d", &N, &M);
 
+	// 미로 입력 & 이동 칸 리셋
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
+			dist[i][j] = -1;
 			scanf("%1d", &maze[i][j]);
 		}
 	}
 
-	visited[0][0] = 1;
 	dist[0][0] = 1;
 	push(0, 0);
 
@@ -54,15 +55,15 @@ int main()
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
-			if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-			if (maze[nx][ny] == 0 || visited[nx][ny]) continue;
+			if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+			if (maze[ny][nx] == 0 || dist[ny][nx] != -1) continue;
 
-			visited[nx][ny] = 1;
-			dist[nx][ny] = dist[x][y] + 1;
-			push(nx, ny);
+			dist[ny][nx] = dist[y][x] + 1;
+			push(ny, nx);
 		}
 	}
 
 	printf("%d", dist[N - 1][M - 1]);
+
 	return 0;
 }
