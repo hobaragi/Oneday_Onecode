e6fb6cc1334ea1b2365322147caaf1f1a9ae1243
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-12-06 10:19:01 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, d, e, f;
+
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	for (int x = -999; x < 1000; x++) {
+		for (int y = -999; y < 1000; y++) {
+			if (a * x + b * y == c && d * x + e * y == f) {
+				printf("%d %d\n", x, y);
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
