219c045fed6ed4e1bf25bc20cb480a2626580af9
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-12-14 14:36:00 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 1568 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.c
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #define MAX 100
 
-int M, N, K;
-int map[MAX][MAX];
-int visited[MAX][MAX];
+int map[MAX][MAX] = { 0 };
+int visited[MAX][MAX] = { 0 };
+int M, N, K, cnt = 0;
+int Count[MAX * MAX];
 
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 
 int dfs(int y, int x) {
-	int cnt = 1;
-
+	int num = 1;
 	visited[y][x] = 1;
 
 	for (int i = 0; i < 4; i++) {
@@ -19,52 +19,28 @@ int dfs(int y, int x) {
 		int ny = y + dy[i];
 
 		if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-		if (map[ny][nx] == 1 || visited[ny][nx]) continue;
+		if (map[ny][nx] != 0 || visited[ny][nx]) continue;
 
-		cnt += dfs(ny, nx);
+		num += dfs(ny, nx);
 	}
 
-	return cnt;
-}
-
-void sort(int Count[], int n) {
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = i + 1; j < n; j++) {
-			if (Count[i] > Count[j]) {
-				int temp = Count[i];
-				Count[i] = Count[j];
-				Count[j] = temp;
-			}
-		}
-	}
+	return num;
 }
 
 int main()
 {
-	// M=y, N=x
 	scanf("%d %d %d", &M, &N, &K);
-
-	// 초기화
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			map[i][j] = 0;
-			visited[i][j] = 0;
-		}
-	}
 
 	for (int i = 0; i < K; i++) {
 		int x1, x2, y1, y2;
 		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 
-		for (int x = x1; x < x2; x++) {
-			for (int y = y1; y < y2; y++) {
+		for (int y = y1; y < y2; y++) {
+			for (int x = x1; x < x2; x++) {
 				map[y][x] = 1;
 			}
 		}
 	}
-
-	int Count[MAX * MAX];
-	int cnt = 0;
 
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) {
@@ -75,7 +51,15 @@ int main()
 		}
 	}
 
-	sort(Count, cnt);
+	for (int i = 0; i < cnt - 1; i++) {
+		for (int j = i + 1; j < cnt; j++) {
+			if (Count[i] > Count[j]) {
+				int temp = Count[i];
+				Count[i] = Count[j];
+				Count[j] = temp;
+			}
+		}
+	}
 
 	printf("%d\n", cnt);
 
