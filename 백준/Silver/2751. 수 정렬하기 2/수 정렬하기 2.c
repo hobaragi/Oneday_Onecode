ccc0e4fb5e4f70ab3a512d954312972928ad2f38
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-12-15 11:40:36 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 400 ms, Memory: 8928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX 1000000
+
+int arr[MAX];
+int temp[MAX];
+
+void merge(int left, int mid, int right) {
+	int i = left;
+	int j = mid + 1;
+	int k = left;
+
+	while (i <= mid && j <= right) {
+		if (arr[i] <= arr[j])
+			temp[k++] = arr[i++];
+		else
+			temp[k++] = arr[j++];
+	}
+
+	while (i <= mid)
+		temp[k++] = arr[i++];
+
+	while (j <= right)
+		temp[k++] = arr[j++];
+
+	for (int t = left; t <= right; t++)
+		arr[t] = temp[t];
+}
+
+void mergeSort(int left, int right) {
+	if (left >= right) return;
+
+	int mid = (left + right) / 2;
+
+	mergeSort(left, mid);
+	mergeSort(mid + 1, right);
+	merge(left, mid, right);
+}
+
+int main()
+{
+	int N;
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	mergeSort(0, N - 1);
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", arr[i]);
+	}
+
+	return 0;
+}
