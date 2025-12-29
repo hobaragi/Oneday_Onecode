f55a1b8cd283e52717a5c2168807569b73021f27
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-12-29 12:40:22 +0900

    [Silver V] Title: 숫자 카드, Time: 296 ms, Memory: 5028 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.c
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.c
@@ -3,42 +3,44 @@
 #include <stdlib.h>
 
 int cmp_int(const void* a, const void* b) {
-    int x = *(const int*)a;
-    int y = *(const int*)b;
-    if (x < y) return -1;
-    if (x > y) return 1;
-    return 0;
+	int x = *(const int*)a;
+	int y = *(const int*)b;
+	if (x < y) return -1;
+	if (x > y) return 1;
+	return 0;
 }
 
 int binary_search(int* arr, int n, int target) {
-    int l = 0, r = n - 1;
-    while (l <= r) {
-        int m = l + (r - l) / 2;
-        if (arr[m] == target) return 1;
-        if (arr[m] < target) l = m + 1;
-        else r = m - 1;
-    }
-    return 0;
+	int l = 0, r = n - 1;
+	while (l <= r) {
+		int m = l + (r - l) / 2;
+		if (arr[m] == target) return 1;
+		if (arr[m] < target) l = m + 1;
+		else r = m - 1;
+	}
+	return 0;
 }
 
-int main(void) {
-    int N, M;
-    scanf("%d", &N);
+int main()
+{
+	int N, M;
 
-    int* cards = (int*)malloc(sizeof(int) * N);
-    for (int i = 0; i < N; i++) scanf("%d", &cards[i]);
+	scanf("%d", &N);
 
-    qsort(cards, N, sizeof(int), cmp_int);
+	int* cards = (int*)malloc(sizeof(int) * N);
+	for (int i = 0; i < N; i++) scanf("%d", &cards[i]);
 
-    scanf("%d", &M);
-    for (int i = 0; i < M; i++) {
-        int x;
-        scanf("%d", &x);
-        printf("%d", binary_search(cards, N, x));
-        if (i != M - 1) printf(" ");
-    }
-    printf("\n");
+	qsort(cards, N, sizeof(int), cmp_int);
 
-    free(cards);
-    return 0;
+	scanf("%d", &M);
+	for (int i = 0; i < M; i++) {
+		int x;
+		scanf("%d", &x);
+		printf("%d ", binary_search(cards, N, x));
+	}
+
+	free(cards);
+	return 0;
+
+	return 0;
 }
