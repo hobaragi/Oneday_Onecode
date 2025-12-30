ddeb4317c8c1313c08404e5408780a8d304950c4
hobaragi <117571999+hobaragi@users.noreply.github.com>
2025-12-30 13:36:37 +0900

    [Silver V] Title: 나이순 정렬, Time: 68 ms, Memory: 21624 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.c
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.c
@@ -1,12 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#define MAX 100000
 
-int age[100000];
-int tmp[100000];
-char name[100000][101];
-char nameTmp[100000][101];
-int N;
+int age[MAX];
+int tmp[MAX];
+char name[MAX][101];
+char nameTmp[MAX][101];
 
 void merge(int left, int mid, int right) {
 	int i = left;
@@ -17,29 +17,25 @@ void merge(int left, int mid, int right) {
 		if (age[i] > age[j]) {
 			tmp[k] = age[j];
 			strcpy(nameTmp[k], name[j]);
-			k++;
-			j++;
+			k++; j++;
 		}
 		else {
 			tmp[k] = age[i];
 			strcpy(nameTmp[k], name[i]);
-			k++;
-			i++;
+			k++; i++;
 		}
 	}
 
 	while (i <= mid) {
 		tmp[k] = age[i];
 		strcpy(nameTmp[k], name[i]);
-		k++;
-		i++;
+		k++; i++;
 	}
 
 	while (j <= right) {
 		tmp[k] = age[j];
 		strcpy(nameTmp[k], name[j]);
-		k++;
-		j++;
+		k++; j++;
 	}
 
 	for (int t = left; t <= right; t++) {
@@ -60,15 +56,19 @@ void mergeSort(int left, int right) {
 
 int main()
 {
+	int N;
+
 	scanf("%d", &N);
 
-	for (int i = 0; i < N;i++)
-		scanf("%d %100s", &age[i], name[i]);
+	for (int i = 0; i < N; i++) {
+		scanf("%d %s", &age[i], &name[i]);
+	}
 
 	mergeSort(0, N - 1);
 
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++) {
 		printf("%d %s\n", age[i], name[i]);
+	}
 
 	return 0;
 }
